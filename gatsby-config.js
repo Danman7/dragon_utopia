068943295bb7c9e2f85ee26837bf46a8be4014f6
Danail Atanasov <danail.atanasov@moneycorp.com>
2020-10-01 18:13:47 +0300

    chore: update strapi source to heroku

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://dashboard.heroku.com/`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`article`, `category`],
         singleTypes: [`home`, `compare-creatures`]
