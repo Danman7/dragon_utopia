90a8b9f96773d9b5a5e9034352d204512a4a8080
Danail Atanasov <danail.atanasov@moneycorp.com>
2020-12-24 14:50:04 +0200

    style: scroll into view animation section

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,10 +1,57 @@
+import { motion, useAnimation } from 'framer-motion'
 import { graphql } from 'gatsby'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
+import { useInView } from 'react-intersection-observer'
 import ReactMarkdown from 'react-markdown'
 
 import Header from '../components/header'
 import Layout from '../components/layout'
+
+const Section = ({ section }) => {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
+  }, [controls, inView])
+
+  return (
+    <motion.div
+      className={section.className || ''}
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={{
+        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0, x: 300 },
+      }}
+    >
+      <ReactMarkdown
+        source={section.content}
+        renderers={{
+          image: ({ src, alt, title }) => {
+            return (
+              <>
+                <a href={src}>
+                  <motion.img
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    src={src}
+                    alt={alt}
+                  />
+                </a>
+                {title && <small className="description">{title}</small>}
+              </>
+            )
+          },
+        }}
+      />
+    </motion.div>
+  )
+}
 
 const ArticleTemplate = ({ data }) => {
   const { title, sections, headerImage, lead } = data.strapiArticle
@@ -19,23 +66,7 @@ const ArticleTemplate = ({ data }) => {
 
       <article>
         {sections.map((section, i) => (
-          <div className={section.className || ''} key={`section-${i}`}>
-            <ReactMarkdown
-              source={section.content}
-              renderers={{
-                image: ({ src, alt, title }) => {
-                  return (
-                    <>
-                      <a href={src}>
-                        <img src={src} alt={alt} />
-                      </a>
-                      {title && <small class="description">{title}</small>}
-                    </>
-                  )
-                },
-              }}
-            />
-          </div>
+          <Section section={section} key={`section-${i}`} />
         ))}
       </article>
     </Layout>
