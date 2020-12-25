250a89287a0e828d9639dddf92f10eb6d67b4ee1
Dan Atanasov <danmanm@gmail.com>
2020-12-25 21:29:16 +0200

    fix: broken section appear animation

--- src/templates/article.js
+++ src/templates/article.js
@@ -25,7 +25,7 @@ const Section = ({ section }) => {
       animate={controls}
       initial="hidden"
       variants={{
-        visible: { opacity: 1, x: 0 },
+        visible: { opacity: 1, y: 0 },
         hidden: { opacity: 0, y: 300 },
       }}
     >
